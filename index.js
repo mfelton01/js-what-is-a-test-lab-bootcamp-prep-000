66695c9621233f5c9d0508a26c3122e7dc178712
Michelle Felton <michelle.l.felton@gmail.com>
2018-06-06 03:02:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // write your code below!
 var name = "Susan"
+<<<<<<< HEAD
 var height = 34
 var message = `${name} is ${height} inches tall`
+=======
+var height = 74
+var message = ` is ${height} inches tall`
+>>>>>>> 095949d10d409dc9636325606da1533b0085cf2c
 
 module.exports = {name, height, message}
